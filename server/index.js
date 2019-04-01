10b0b44bc46e5a46e69b45b8dac8fa8ba90d89e5
Cody Smith <cms287@gmail.com>
2019-04-01 12:10:35 -0700

    basic layout with css grid

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
-const port = 3006
+const app = express();
+const port = 3006;
 
 const db = require('../database/index.js');
 
