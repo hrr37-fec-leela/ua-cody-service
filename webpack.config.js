36a0f4ed4e41f3252e3623c610fac2ce78d19959
Cody Smith <cms287@gmail.com>
2019-04-10 17:39:20 -0700

    "renamed bundle to bundle-cody for proxy"

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle-cody.js',
     path: __dirname + '/public/dist'
   }
 };
